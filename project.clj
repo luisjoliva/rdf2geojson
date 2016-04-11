0c16199adbe18ed34b7bea54a9904c5cf62dcabc
Luis Oliva <loliva@lsi.upc.edu>
2016-04-11 18:36:52 +0200

    Adds newest clojure version and data.json dependency to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject rdf2geojson "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Transforms RDF triples into geojson format."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.4"]])
